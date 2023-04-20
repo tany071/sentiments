683bfd540b2f666cfe7815c530e6be38673ed0b1
utkarsh575 <utkarshlohani575@gmail.com>
2023-04-20 12:12:09 +0530

    :sparkles: added contact section

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,16 @@
 import { NextPage } from "next";
+import Image from "next/image";
 
-const contact : NextPage =()=>{
-
-    return (
-        <>contact</>
-    )
-}
+const contact: NextPage = () => {
+  return (
+    <>
+      <section className="h-full bg-gray-100 border-purple-400 border-2 mx-10 my-10 flex items-center justify-center rounded-xl gap-5  ">
+        <div className="flex flex-wrap justify-center items-center mx-10 my-10 gap-10">
+          <Image className="h-full w-full" src="/gethelp.jpg" alt="helplines" width={700} height={500} />
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default contact;
